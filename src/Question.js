b6be6b6ab1a43c62a8a4aefde2caff654f0f6f18
Jonathan Moore <jmjonemoore@gmail.com>
2019-05-13 16:17:45 -0500

    Modified question.js

--- src/Question.js
+++ src/Question.js
@@ -1,24 +1,7 @@
 
 class Question {
     constructor(){
-        this.text = '',
-        this.answers = [],
-        this.correctAnswer = '',
-        this.userAnswer = '';
     }
-
-    submitAnswer(answer){
-        this.userAnswer = answer;
-    }
-
-    answerStatus(){
-        if (this.userAnswer === this.correctAnswer){
-            return true;
-        }else {
-            return false;
-        }
-    }
-
 }
 
 export default Question;
