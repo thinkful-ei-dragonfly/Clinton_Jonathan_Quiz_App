bc14d3a5b210222fd0cd0ae0d7e793ac2188b23e
Jonathan Moore <jmjonemoore@gmail.com>
2019-05-14 09:33:42 -0500

    Updated Engine functions

--- src/Quiz.js
+++ src/Quiz.js
@@ -20,25 +20,47 @@ class Quiz {
 
   // Example method:
   startGame() {
+    this.asked = [];
     this.active = true;
     const trivia = new TriviaApi();
     trivia.getQuestions()
-    .then(res => res.forEach(question =>{
-      this.unasked.push(question);
-      this.asked.push(this.unasked.shift());
-    }));
-  }
-  
+    .then(res => {
+        res.results.forEach(item => {
+          let question = new Question();
+          question.text = item.question;
+          question.answers = [item.correct_answer, ...item.incorrect_answers];
+          question.correctAnswer = item.correct_answer;
+          question.userAnswer = '';
+
+          this.unasked.push(question);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
 
   nextQuestion(){
-    this.asked.push(this.unasked.shift());
+    this.asked.unshift(this.unasked.shift());
   }
 
-  // submitAnswer(answer) {
-  //   question.userAnswer = answer;
-  // }
- 
+  updateScoreHistory(){
+    if (this.asked.length === 5) {
+      this.scoreHistory.push(this.score);
+    }
+  }
+
+  endgame(){
+    this.active = false;
+    this.score = 0;
+  }
+
+  updateScore(){
+    if (this.asked[0].userAnswer === this.asked[0].correct_answer) {
+      this.score ++;
+  } 
+}
 }
 
 export default Quiz;
--- src/TriviaApi.js
+++ src/TriviaApi.js
@@ -1,16 +1,18 @@
 'use strict';
 
-import Quiz from './Quiz';
-
 class TriviaApi {
   
   static BASE_URL = 'https://opentdb.com/api.php?amount=5';
 
   getQuestions(){
-    const url = new URL(TriviaApi.BASE_URL);
-    return fetch(url)
-    .then(res => res.json())
-    .then(res => res.results);
+    return fetch(`${TriviaApi.BASE_URL}`)
+      .then(res => 
+      { 
+        return res.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
 
