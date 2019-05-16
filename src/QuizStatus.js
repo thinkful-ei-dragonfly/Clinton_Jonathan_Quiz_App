190d4603e23e8e242ecc1a00836d5ba5277bc6db
Jonathan Moore <jmjonemoore@gmail.com>
2019-05-16 14:41:36 -0500

    Added generateProgress() to QuizStatus.js

--- src/QuizStatus.js
+++ src/QuizStatus.js
@@ -14,8 +14,19 @@ class QuizStatus extends Renderer {
       <p>High Score: ${this.model.highestScore}</p>
       </div>
       <div>
-      <p>Progress: ${this.model.asked.length} of 5</p>
+      <p>Progress: ${this.generateProgress()}</p>
     `;
+  }
+
+  generateProgress(){
+    let progressHtml;
+    if(this.model.asked.length === 0){
+      progressHtml = 'Inactive';
+    }
+    else{
+      progressHtml = `${this.model.asked.length} of 5`;
+    }
+    return progressHtml;
   }
 
 }
