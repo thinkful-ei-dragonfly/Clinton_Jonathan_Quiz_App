8dbe4816330f137e9e425d8261f14af991bc3af1
Jonathan Moore <jmjonemoore@gmail.com>
2019-05-17 19:18:55 -0500

    Added some styling

--- src/QuizStatus.js
+++ src/QuizStatus.js
@@ -7,14 +7,15 @@ class QuizStatus extends Renderer {
     // return some HTML here, utilizing `this.model`
 
     return `
-      <div>
+      <div class="status-score">
       <p>Score: ${this.model.score}</p>
       </div>
-      <div>
+      <div class="status-highest-score">
       <p>High Score: ${this.model.highestScore}</p>
       </div>
-      <div>
+      <div class="status-progress">
       <p>Progress: ${this.generateProgress()}</p>
+      </div>
     `;
   }
 
